f8b62d37e69865dacabf4e1fc888b7ee729fd0e8
Chris Paterson <chrispaterson22@gmail.com>
2021-05-08 15:16:27 +0100

    styling done with dummy contents

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './App.css'
 function App() {
 	return (
 		<div className='App card shadow'>
-			<div className='card-body'>
-				<i class='fas fa-quote-left'></i>
-				<blockquote className='blockquote mb-0'>
+			<div className='quote-container'>
+				<i className='fas fa-quote fa-quote-left'></i>
+
+				<p className='blockquote mb-0'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
 					repudiandae aliquid commodi numquam. Natus dolores vero adipisci
 					itaque perspiciatis facere, exercitationem tempora deleniti nisi ipsa,
 					non optio error incidunt atque nam voluptates beatae at blanditiis
 					fugiat possimus quaerat! Magni itaque alias dolor amet vel iure,
 					impedit voluptatem consequuntur maiores nihil!
-				</blockquote>
-				<i class='fas fa-quote-right'></i>
-				<footer class='blockquote-footer'>
-					Someone famous in <cite title='Source Title'>Source Title</cite>
-				</footer>
+				</p>
+				<i className='fas fa-quote fa-quote-right'></i>
+			</div>
+			<div className='quote-author'>
+				<span className='blockquote-footer'>Who said this</span>
+			</div>
+			<div className='btn-container'>
+				<button
+					type='button'
+					className='tweet-btn btn btn-outline-primary rounded-circle'
+				>
+					<i className='fab fa-twitter'></i>
+				</button>
+				<button
+					type='button'
+					className='new-quote-btn btn btn-outline-primary rounded-pill'
+				>
+					New Quote
+				</button>
 			</div>
 		</div>
 	)
