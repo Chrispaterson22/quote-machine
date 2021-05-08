84a1d6cc44f293fa85e853c11e43de913a5cebe9
Chris Paterson <chrispaterson22@gmail.com>
2021-05-08 14:22:29 +0100

    WIP styling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import './App.css'
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+		<div className='App card shadow'>
+			<div className='card-body'>
+				<i class='fas fa-quote-left'></i>
+				<blockquote className='blockquote mb-0'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+					repudiandae aliquid commodi numquam. Natus dolores vero adipisci
+					itaque perspiciatis facere, exercitationem tempora deleniti nisi ipsa,
+					non optio error incidunt atque nam voluptates beatae at blanditiis
+					fugiat possimus quaerat! Magni itaque alias dolor amet vel iure,
+					impedit voluptatem consequuntur maiores nihil!
+				</blockquote>
+				<i class='fas fa-quote-right'></i>
+				<footer class='blockquote-footer'>
+					Someone famous in <cite title='Source Title'>Source Title</cite>
+				</footer>
+			</div>
 		</div>
 	)
 }
